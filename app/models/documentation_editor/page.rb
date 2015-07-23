d70351e5cb8f5a53d78a8f6377dcaff49d936bb5
Sylvain UTARD <sylvain.utard@gmail.com>
2015-07-22 20:01:37 -0700

    Ability to generate the menu

--- app/models/documentation_editor/page.rb
+++ app/models/documentation_editor/page.rb
@@ -5,9 +5,33 @@ module DocumentationEditor
       document.to_html
     end
 
+    def to_menu
+      roots = []
+      levels = []
+      document.root.children.each do |child|
+        next if child.type != :header
+        item = { label: child.options[:raw_text], id: generate_id(child.options[:raw_text]), children: [], level: child.options[:level] }
+        while !levels.empty? && levels.last[:level] >= child.options[:level]
+          levels.pop
+        end
+        if levels.empty?
+          roots << item
+        else
+          levels.last[:children] << item
+        end
+        levels << item
+      end
+      roots
+    end
+
     private
     def document
       @document ||= Kramdown::Document.new(content, :input => 'ReadmeIOKramdown')
+    end
+
+    def generate_id(str)
+      @html_converter ||= Kramdown::Converter::Html.send(:new, document.root, {auto_id_prefix: ''})
+      @html_converter.generate_id(str)
     end
 
   end
