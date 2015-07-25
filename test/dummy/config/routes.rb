c3d1c2f7ea2da02566ebeeec6dbaee1191b9e97f
Sylvain UTARD <sylvain.utard@gmail.com>
2015-07-24 19:28:19 -0700

    Try with another route

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get '/doc/ruby', controller: 'documentation_editor/pages', action: 'show', slug: 'ruby', condition: 'ruby', language: 'ruby'
+  get '/doc/javascript', controller: 'documentation_editor/pages', action: 'show', slug: 'ruby', condition: 'js', language: 'js'
   mount DocumentationEditor::Engine => "/doc"
 
 end
