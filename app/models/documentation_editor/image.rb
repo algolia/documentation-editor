2bb23a7781b527b4740e2daeb00eb90e26d0d9da
Sylvain UTARD <sylvain.utard@gmail.com>
2015-07-25 11:23:53 -0700

    Pluralize the paperclip_option setting

--- app/models/documentation_editor/image.rb
+++ app/models/documentation_editor/image.rb
@@ -1,6 +1,6 @@
 module DocumentationEditor
   class Image < ActiveRecord::Base
-    has_attached_file(:image, DocumentationEditor::Config.paperclip_option || {})
+    has_attached_file(:image, DocumentationEditor::Config.paperclip_options || {})
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   end
 end
