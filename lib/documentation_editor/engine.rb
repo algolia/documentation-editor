b472797050399c48a89458d4c1e771b2b9586ecc
Sylvain UTARD <sylvain.utard@gmail.com>
2015-08-02 17:46:53 -0700

    Rails 4 compliance

--- lib/documentation_editor/engine.rb
+++ lib/documentation_editor/engine.rb
@@ -9,7 +9,9 @@ module DocumentationEditor
 
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
-        app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
       end
     end
   end
