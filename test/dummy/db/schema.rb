09931da86b2bd3f39761e313bace58f4d9d28fe4
Sylvain UTARD <sylvain.utard@gmail.com>
2015-08-20 16:31:48 +0200

    Updated schema.rb

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818103106) do
+ActiveRecord::Schema.define(version: 20150818102838) do
 
   create_table "documentation_editor_images", force: :cascade do |t|
     t.string   "caption"
