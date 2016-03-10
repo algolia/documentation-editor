49d4aaf754c9f1a887812ae566ed6b632270975c
Sylvain UTARD <sylvain.utard@gmail.com>
2016-03-10 10:57:13 +0100

    Testing purpose

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get '/doc/ruby(/:section)', controller: 'documentation_editor/pages', action: 'show', slug: 'guide', language: 'ruby'
   get '/doc/javascript(/:section)', controller: 'documentation_editor/pages', action: 'show', slug: 'guide', language: 'js'
+  get '/doc/guide/:language', controller: 'documentation_editor/pages', action: 'show', slug: 'guide'
   mount DocumentationEditor::Engine => "/doc"
 
 end
