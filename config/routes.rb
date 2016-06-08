edf53ba21e6bd747bee7c2bd89be387ea0b2306b
Sylvain UTARD <sylvain.utard@gmail.com>
2016-06-08 17:00:13 +0200

    Ensure all routes only accept HTML

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,27 @@
 DocumentationEditor::Engine.routes.draw do
-  scope path: '/admin' do
-    get '/', as: :admin, :controller => 'pages', :action => 'index'
-    post '/', :controller => 'pages', :action => 'create'
+  constraints format: 'html' do
+    scope path: '/admin' do
+      get '/', as: :admin, :controller => 'pages', :action => 'index'
+      post '/', :controller => 'pages', :action => 'create'
 
-    post '/images', as: :upload_image, :controller => 'pages', :action => 'upload_image'
+      post '/images', as: :upload_image, :controller => 'pages', :action => 'upload_image'
 
-    get '/export', as: :export, :controller => 'pages', :action => 'export'
-    post '/import', as: :import, :controller => 'pages', :action => 'import'
+      get '/export', as: :export, :controller => 'pages', :action => 'export'
+      post '/import', as: :import, :controller => 'pages', :action => 'import'
 
-    get '/:id', :controller => 'pages', :action => 'source'
-    get '/:id/edit', as: :edit_page, :controller => 'pages', :action => 'edit'
-    put '/:id', as: :update, :controller => 'pages', :action => 'update'
-    post '/:id', :controller => 'pages', :action => 'commit'
-    post '/:id/thumbnail', as: :upload_thumbnail, :controller => 'pages', :action => 'upload_thumbnail'
-    delete '/:id', as: :delete_page, :controller => 'pages', :action => 'destroy'
-    get '/:id/preview', as: :preview_page, :controller => 'pages', :action => 'preview'
-    get '/:prev/:cur/diff', :controller => 'pages', :action => 'diff'
-    get '/:id/history', as: :history, :controller => 'pages', :action => 'history'
-    get '/:id/versions', :controller => 'pages', :action => 'versions'
+      get '/:id', :controller => 'pages', :action => 'source'
+      get '/:id/edit', as: :edit_page, :controller => 'pages', :action => 'edit'
+      put '/:id', as: :update, :controller => 'pages', :action => 'update'
+      post '/:id', :controller => 'pages', :action => 'commit'
+      post '/:id/thumbnail', as: :upload_thumbnail, :controller => 'pages', :action => 'upload_thumbnail'
+      delete '/:id', as: :delete_page, :controller => 'pages', :action => 'destroy'
+      get '/:id/preview', as: :preview_page, :controller => 'pages', :action => 'preview'
+      get '/:prev/:cur/diff', :controller => 'pages', :action => 'diff'
+      get '/:id/history', as: :history, :controller => 'pages', :action => 'history'
+      get '/:id/versions', :controller => 'pages', :action => 'versions'
+    end
+
+    get '/:slug/sections/:section', as: :section, :controller => 'pages', :action => 'show', constraints: { slug: /.*/ }
+    get '/:slug', as: :page, :controller => 'pages', :action => 'show', constraints: { slug: /.*/ }
   end
-
-  get '/:slug/sections/:section', as: :section, :controller => 'pages', :action => 'show', constraints: { slug: /.*/ }
-  get '/:slug', as: :page, :controller => 'pages', :action => 'show', constraints: { slug: /.*/ }
 end
